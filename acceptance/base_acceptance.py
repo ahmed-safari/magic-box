afc4fe6342b9913b146d4794426e51f298d03f6e
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-05 18:25:58 +0300

    added max_stuck_count

--- acceptance/base_acceptance.py
+++ acceptance/base_acceptance.py
@@ -1,6 +1,7 @@
 class BaseAcceptance:
-    def __init__(self):
+    def __init__(self, max_stuck_count=10):
         self.stuck_count = 0
+        self.max_stuck_count = max_stuck_count
 
     def accept(self, new_cost, old_cost):
         raise NotImplementedError()
