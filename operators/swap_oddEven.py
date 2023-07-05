cafc2fddbc6e1ce3c84e3b82fe29674a4d4cc4c4
NajlaZuhir <60101292@udst.edu.qa>
2023-07-05 11:07:37 +0300

    odd and even

---
+++ operators/swap_oddEven.py
@@ -0,0 +1,47 @@
+from .base_operator import BaseOperator
+import random
+class SwapOddEven(BaseOperator):
+    def __init__(self):
+        super(  ).__init__() # Call the constructor of the parent class (to initialize self.score)
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0 
+        self.y2 = 0 
+
+    def apply(self, box):
+        # Get the size of the box
+        n = len(box)
+        # Generate two random positions in the box
+        self.x1 = random.randint(0,n - 1)
+        self.y1 = random.randint(0,n - 1)
+
+        num = box[self.x1][self.y1]
+        if num % 2 == 0:
+            while True:
+                self.x2 = random.randint(0,n - 1)
+                self.y2 = random.randint(0,n - 1)
+                nextNum = box[self.x2][self.y2]
+
+                if nextNum % 2 == 0 and nextNum != num:
+                    break
+
+        else:
+            while True:
+                self.x2 = random.randint(0,n - 1)
+                self.y2 = random.randint(0,n - 1)
+                nextNum = box[self.x2][self.y2]
+
+                if nextNum % 2 != 0 and nextNum != num:
+                    break
+
+
+
+        # Swap the elements
+        box[self.x1][self.y1], box[self.x2][self.y2] = box[self.x2][self.y2], box[self.x1][self.y1]
+        # Note: we do not need to return the box, because we are modifying the box in-place
+
+
+    def revert(self, box):
+        # Swap back the elements with the same indexes (we did not generate new indexes, so we can just swap back)
+        box[self.x1][self.y1], box[self.x2][self.y2] = box[self.x2][self.y2], box[self.x1][self.y1] 
+        # Note: we do not need to return the box, because we are modifying the box in-place
