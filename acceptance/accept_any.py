f90ee6239c9e2d833f52a42118aed660deb27af6
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-07 13:08:57 +0300

    removed useless constructor

--- acceptance/accept_any.py
+++ acceptance/accept_any.py
@@ -2,8 +2,5 @@ from .base_acceptance import BaseAcceptance
 
 
 class AcceptAny(BaseAcceptance):
-    def __init__(self):
-        super().__init__()  # call the parent constructor (to initialize self.stuck_count)
-
     def accept(self, new_cost, old_cost):
         return True  # Always accept the new solution
