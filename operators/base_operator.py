47c85f96251c2a8ebfcd8960af2b9610f4dec447
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-07 07:09:01 +0300

    keep track of selection and accwptance counts

--- operators/base_operator.py
+++ operators/base_operator.py
@@ -1,9 +1,17 @@
 class BaseOperator:
     def __init__(self):
         self.score = 0
+        self.selected_count = 0
+        self.accepted_count = 0
 
     def apply(self, box):
         raise NotImplementedError()
+
+    def update_selected(self):
+        self.selected_count += 1
+
+    def update_accepted(self):
+        self.accepted_count += 1
 
     def update_score(self, change):
         self.score += change
