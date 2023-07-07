7cd1e2e7a48cb52c99da182c3a342c83d9100474
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-07 08:03:02 +0300

    seperated selection method from solver

--- selection/reinforcement_selection.py
+++ selection/reinforcement_selection.py
@@ -3,53 +3,28 @@ from acceptance import *
 from .base_selection import BaseSelection
 
 
-class ReinforcementSolver(BaseSelection):
+class ReinforcementSelection(BaseSelection):
     def __init__(self, llh_list, max_iterations, training_percentage=0.1):
         super().__init__(llh_list, max_iterations)
         self.training_percentage = training_percentage
+        self.llh_scores = [0.0] * len(self.llh_list)  # List of scores for each LLH
 
-    def solve(self):
-        # Create the list with scores of operators initalized to 0
+    def select_operator(self, new_cost, old_cost, did_accept):
+        # If we are in the training phase, select an operator in a round robin fashion
+        if self.iteration < self.max_iterations * self.training_percentage:
+            operator_index = self.iteration % len(self.llh_scores)
+        # Else, select the LLH with the highest score
+        else:
+            operator_index = self.llh_scores.index(max(self.llh_scores))
 
-        # operators_scores = [0 for _ in range(len(self.llh))]
-        operators_scores = [0] * len(self.llh_list)
+        self.operator = self.llh_list[operator_index]
 
-        for i in range(self.max_iterations):
-            if self.cost == 0:
-                return self.solution  # return the solution if found
+        # Update the score of the operator
+        if did_accept:
+            self.llh_scores[operator_index] += 1
+            self.operator.update_score(1)
+        else:
+            self.llh_scores[operator_index] -= 1
+            self.operator.update_score(-1)
 
-            # if we are in the first 20% of the iterations, select an operator in a round robin fashion
-            if i < self.max_iterations * self.training_percentage:
-                # print("round robin")
-                operator_index = i % len(operators_scores)
-                operator = self.llh_list[operator_index]
-                # print(operator.__class__.__name__)
-
-            # Else, select the LLH with the highest score
-            else:
-                # print("highest score")
-                operator_index = operators_scores.index(max(operators_scores))
-                operator = self.llh_list[operator_index]
-                # print(operator.__class__.__name__)
-
-            # Apply the operator
-            operator.apply(self.solution)
-
-            # Calculate the new cost
-            new_cost = calculate_objective(self.solution)
-
-            # Check if the new solution is better than the current best one (and update it if so)
-            self.check_best(new_cost, self.solution, i)
-
-            if self.acceptance.accept(new_cost=new_cost, old_cost=self.cost):
-                self.cost = new_cost
-                operators_scores[
-                    operator_index
-                ] += 1  # incrementing the score of the operator
-                operator.update_score(1)  # incrementing the score of the operator
-            else:
-                operator.revert(self.solution)
-                operators_scores[operator_index] -= 1
-                operator.update_score(-1)  # decrementing the score of the operator
-
-        return self.solution
+        return self.operator
