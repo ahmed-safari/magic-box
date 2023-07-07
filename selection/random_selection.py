bc1e345b52095577372b4b414ff1f1da4e28b247
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-07 13:11:49 +0300

    fixed selection_count not being updated

--- selection/random_selection.py
+++ selection/random_selection.py
@@ -4,4 +4,5 @@ import random
 
 class RandomSelection(BaseSelection):
     def select_operator(self, new_cost, old_cost, did_accept, current_iteration):
-        return random.choice(self.llh_list)
+        self.operator = random.choice(self.llh_list)
+        return self.operator
