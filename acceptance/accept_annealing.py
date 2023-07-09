cbbe30c8811900cd728699467603089f56f7fd93
Sp0derDev <Sp0derDev@protonmail.com>
2023-07-09 09:19:48 +0300

    changed logic

--- acceptance/accept_annealing.py
+++ acceptance/accept_annealing.py
@@ -1,13 +1,15 @@
 from .base_acceptance import BaseAcceptance
 import random
-import math
+import numpy as np
 
 
 class AcceptAnnealing(BaseAcceptance):
     def accept(self, new_cost, old_cost, current_iteration):
-        acceptance_probability = math.exp(
-            ((old_cost - new_cost) / current_iteration) * -1
-        )
+        if new_cost < old_cost:  # If the new solution is better than the old one
+            return True  # Accept the new solution
+
+        acceptance_probability = np.exp(((old_cost - new_cost) / current_iteration))
         if random.random() < acceptance_probability:
             return True
+
         return False
